6d775ff7996329c36e60c7636965be7b2fb9991f
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2021-04-30 13:45:06 -0300

    Fixes, review

--- src/scripts/leitos/_leitos.js
+++ src/scripts/leitos/_leitos.js
@@ -3,6 +3,8 @@ let attempts = 0
 let success = false
 const maxAttempts = 3
 const dataUrl = "https://leitos-hospitalares.s3.amazonaws.com/metrics_by_region_latest.json"
+const okPink = "#d01c8b"
+const okGreen = "#00cb8d"
 
 document.addEventListener("DOMContentLoaded", function() {
   getDataAndInit()
@@ -38,18 +40,20 @@ function init(allData, region) {
     <!-- Dados gerais -->
     <div class="row">
       <div class="col-xs-12">
-        <!-- <h2 class="t6 bb">Dados gerais - ${latest.region}</h2> -->
         <h2 class="t6 bb">Dados gerais</h2>
       </div>
     </div>
     <div class="row mt1">
       <div class="col-sm-6 col-xs-12 mt1">
         <h5>Total de estabelecimentos hospitalares</h5>
-        <h4 class="t5">${latest.total_hospitals.toLocaleString()}</h4>
+        <h4 class="t5">${Number(latest.total_hospitals).toLocaleString("pt-BR", {maximumFractionDigits: 0})}</h4>
       </div>
       <div class="col-sm-6 col-xs-12 mt1">
-        <h5>Painel atualizado em</h5>
-        <h4 class="t5">${moment(allData.ts_run).format('DD/MM/YYYY hh:mm:ss')}</h4>
+        <div class="pill">
+          <h5>Painel atualizado em</h5>
+          <h4 class="t5">${moment(allData.ts_run).format('DD/MM/YYYY ')}</h4>
+          <h6 class="t3">Às ${moment(allData.ts_run).format('hh:mm:ss')}</h6>
+        </div>
       </div>
     </div>
 
@@ -62,16 +66,10 @@ function init(allData, region) {
     <div class="row mt1">
       <div class="col-sm-6 col-xs-12 mt1">
         <h5>Estabelecimentos desatualizados há 7 dias ou mais</h5>
-        <!--
-        <h4 class="t5">${(latest.pct_oudated_7d * 100).toLocaleString('pt-BR', {maximumFractionDigits: 0})}%</h4>
-        -->
         ${gaugeChart(latest.pct_oudated_7d, valueChange(latest, previous, 'pct_oudated_7d', false))}
       </div>
       <div class="col-sm-6 col-xs-12 mt1">
         <h5>Estabelecimentos desatualizados há 90 dias ou mais</h5>
-        <!--
-        <h4 class="t5">${(latest.pct_oudated_90d * 100).toLocaleString('pt-BR', {maximumFractionDigits: 0})}%</h4>
-        -->
         ${gaugeChart(latest.pct_oudated_90d, valueChange(latest, previous, 'pct_oudated_90d', false))}
       </div>
     </div>
@@ -92,16 +90,22 @@ function init(allData, region) {
     </div>
     <div class="row mt1">
       <div class="col-sm-6 col-xs-12 mt1">
-        <h5>Estabelecimentos com problemas na taxa de ocupação (acima de 120%)</h5>
-        <!--
-        <h4 class="t5">${(latest.pct_uti_ocup_gt120 * 100).toLocaleString('pt-BR', {maximumFractionDigits: 0})}%</h4>
-        -->
-          ${gaugeChart(latest.pct_uti_ocup_gt120, valueChange(latest, previous, 'pct_uti_ocup_gt120', false))}
+        <h5>Estabelecimentos com problemas na taxa de ocupação (acima de 120%)*</h5>
+        ${gaugeChart(latest.pct_uti_ocup_gt120, valueChange(latest, previous, 'pct_uti_ocup_gt120', false))}
+        <p class="mt3">*Percentual de estabelecimentos que possuem taxas de ocupação de UTI acima de 120%, indicando problema na qualidade dos dados</p>
       </div>
       <div class="col-sm-6 col-xs-12 mt1">
         <h5>Taxa de ocupação atual</h5>
-        <h4 class="t5">${(latest.tx_ocup_srag_uti * 100).toLocaleString('pt-BR', {maximumFractionDigits: 0})}% ${valueChange(latest, previous, 'tx_ocup_srag_uti', false)} (Covid)</h4>
-        <h4 class="t5">${(latest.tx_ocup_hosp_uti * 100).toLocaleString('pt-BR', {maximumFractionDigits: 0})}% ${valueChange(latest, previous, 'tx_ocup_hosp_uti', false)} (Não-Covid)</h4>
+        <h4 class="t5">${(latest.tx_ocup_srag_uti * 100).toLocaleString("pt-BR", {maximumFractionDigits: 0})}% ${valueChange(latest, previous, 'tx_ocup_srag_uti', false)} (Covid)</h4>
+        <h4 class="t5">${(latest.tx_ocup_hosp_uti * 100).toLocaleString("pt-BR", {maximumFractionDigits: 0})}% ${valueChange(latest, previous, 'tx_ocup_hosp_uti', false)} (Não-Covid)</h4>
+      </div>
+    </div>
+    <div class="row mt3">
+      <div class="col-xs-12">
+        <h5>Estabelecimentos com problemas na taxa de ocupação - Série histórica</h5>
+        <div style="height: 300px;">
+          <canvas id="occupationWithProblems"></canvas>
+        </div>
       </div>
     </div>
     <div class="row mt3">
@@ -122,13 +126,13 @@ function init(allData, region) {
     <div class="row mt1">
       <div class="col-sm-6 col-xs-12 mt1">
         <h5>Leitos de UTI existentes</h5>
-        <h4 class="t5">${Math.round(latest.ocup_srag_uti).toLocaleString()} (Covid)</h4>
-        <h4 class="t5">${Math.round(latest.ocup_hosp_uti).toLocaleString()} (Não-Covid)</h4>
+        <h4 class="t5">${Math.round(latest.ocup_srag_uti).toLocaleString("pt-BR")} (Covid)</h4>
+        <h4 class="t5">${Math.round(latest.ocup_hosp_uti).toLocaleString("pt-BR")} (Não-Covid)</h4>
       </div>
       <div class="col-sm-6 col-xs-12 mt1">
         <h5>Leitos de UTI ocupados</h5>
-        <h4 class="t5">${Math.round(latest.oferta_srag_uti).toLocaleString()} (Covid)</h4>
-        <h4 class="t5">${Math.round(latest.oferta_hosp_uti).toLocaleString()} (Não-Covid)</h4>
+        <h4 class="t5">${Math.round(latest.oferta_srag_uti).toLocaleString("pt-BR")} (Covid)</h4>
+        <h4 class="t5">${Math.round(latest.oferta_hosp_uti).toLocaleString("pt-BR")} (Não-Covid)</h4>
       </div>
     </div>
     <div class="row mt3">
@@ -143,13 +147,37 @@ function init(allData, region) {
 
   lineChart("pctOutdatedChart",
     data.map(a => moment(a.ts_run).format("DD/MM/YYYY")).reverse(),
-    [{
-      label: "% desatualizado há 7 dias ou mais",
-      data: data.map(a => (Math.round(a.pct_oudated_7d * 1000) / 10)).reverse(),
-      fill: false,
-      borderColor: "#00cb8d",
-      tension: 0.1
-    }]
+    [
+        {
+        label: "% desatualizado há 7 dias ou mais",
+        data: data.map(a => (Math.round(a.pct_oudated_7d * 1000) / 10)).reverse(),
+        fill: false,
+        borderColor: okGreen,
+        tension: 0.1
+      },
+      {
+        label: "% desatualizado há 90 dias ou mais",
+        data: data.map(a => (Math.round(a.pct_oudated_90d * 1000) / 10)).reverse(),
+        fill: false,
+        borderColor: okPink,
+        tension: 0.1
+      },
+    ],
+    true
+  )
+
+  lineChart("occupationWithProblems",
+    data.map(a => moment(a.ts_run).format("DD/MM/YYYY")).reverse(),
+    [
+      {
+        label: "% de estabelecimentos com problemas na taxa de ocupação",
+        data: data.map(a => (Math.round(a.pct_uti_ocup_gt120 * 1000) / 10)).reverse(),
+        fill: false,
+        borderColor: okPink,
+        tension: 0.1
+      },
+    ],
+    true
   )
 
   lineChart("occupationRateChart",
@@ -159,17 +187,18 @@ function init(allData, region) {
         label: "% ocupação UTI Covid",
         data: data.map(a => (Math.round(a.tx_ocup_srag_uti * 1000) / 10)).reverse(),
         fill: false,
-        borderColor: "#00cb8d",
+        borderColor: okGreen,
         tension: 0.1
       },
       {
         label: "% ocupação UTI Não-Covid",
         data: data.map(a => (Math.round(a.tx_ocup_hosp_uti * 1000) / 10)).reverse(),
         fill: false,
-        borderColor: "#d01c8b",
+        borderColor: okPink,
         tension: 0.1
       }
-    ]
+    ],
+    true
   )
 
   lineChart("numberOfBeds",
@@ -179,17 +208,18 @@ function init(allData, region) {
         label: "Oferta - Total de Leitos UTI (Covid e Não-Covid)",
         data: data.map(a => Math.round(a.oferta_total_uti)).reverse(),
         fill: false,
-        borderColor: "#00cb8d",
+        borderColor: okGreen,
         tension: 0.1
       },
       {
         label: "Ocupação - Total de Leitos UTI (Covid e Não-Covid)",
         data: data.map(a => Math.round(a.ocup_total_uti)).reverse(),
         fill: false,
-        borderColor: '#d01c8b',
+        borderColor: okPink,
         tension: 0.1
       }
-    ]
+    ],
+    true
   )
 
   $("#regionSelect").on("change", function() {
@@ -198,7 +228,7 @@ function init(allData, region) {
 
 }
 
-function lineChart(id, labels, datasets) {
+function lineChart(id, labels, datasets, beginAtZero) {
   const chartData = {
     labels: labels,
     datasets: datasets,
@@ -215,6 +245,11 @@ function lineChart(id, labels, datasets) {
           display: true,
           position: "bottom"
         }
+      },
+      scales: {
+        y: {
+          beginAtZero: beginAtZero,
+        }
       }
     },
   };
@@ -228,9 +263,9 @@ function gaugeChart(value, arrow) {
   return (`
     <div class="sc-gauge mt1">
         <div class="sc-background">
-          <div class="sc-percentage" style="--rotation: ${(value * 180)}deg; --background: red;"></div>
+          <div class="sc-percentage" style="--rotation: ${(value * 180)}deg; --background: ${okPink};"></div>
           <div class="sc-mask"></div>
-          <span class="sc-value">${(value * 100).toLocaleString('pt-BR', {maximumFractionDigits: 0})}%${arrow}</span>
+          <span class="sc-value">${(value * 100).toLocaleString("pt-BR", {maximumFractionDigits: 0})}%${arrow}</span>
         </div>
         <span class="sc-min">0</span>
         <span class="sc-max">100</span>
@@ -242,7 +277,7 @@ function valueChange(latest, previous, column, upIsGood) {
   const valLatest = (Math.round(latest[column] * 1000) / 10)
   const valPrevious = (Math.round(previous[column] * 1000) / 10)
   if (valLatest === valPrevious) {
-    return "<span></span>"
+    return "<span>&nbsp;&ndash;</span>"
   } else if (valLatest > valPrevious) {
     return `<span class="${upIsGood ? 'change-good' : 'change-bad'}">&#9650;</span>` // up
   } else if (valLatest < valPrevious) {
@@ -261,7 +296,7 @@ function generateSelect(region) {
   ]
   const options = regions.map(a => `<option value="${a}" ${a === region ? "selected" : ""}>${a}</option>`)
   return (`<div class="mb2">
-    <label>Filtre por região</label>
+    <label><strong>Filtre por região</strong></label>
     <select id="regionSelect">
       ${options.join('\n')}
     </select>
