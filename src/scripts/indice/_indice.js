b124347bf4ac4b2dc5614821119eef4d72c03665
liaporto <moonkoala23@gmail.com>
2021-08-19 17:17:46 -0300

    torna estados a visao padrao e corrige gulpfile.js

--- src/scripts/indice/_indice.js
+++ src/scripts/indice/_indice.js
@@ -186,7 +186,7 @@ var app = {
   dados: false,
   avaliacao: 1,
   posicoes: [],
-  instancia: "capitais",
+  instancia: "estados",
   carregar: function() {
     $.getJSON('./assets/base/' + app.instancia + '.json', function(data) {
       app.dados = data;
